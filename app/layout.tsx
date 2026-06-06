1424c5091b80e32f47d042214c18950f8b200a78
blunts954 <blunts954@gmail.com>
2026-06-06 15:01:29 -0700

    Add Microsoft Clarity analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,6 +101,9 @@ gtag('config', '${GA_MEASUREMENT_ID}', { send_page_view: true });`}
         <CipherChat />
         <Analytics />
         <SpeedInsights />
+        <Script id="ms-clarity" strategy="afterInteractive" nonce={nonce}>
+          {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y)})(window,document,"clarity","script","x2zh9pih5y");`}
+        </Script>
       </body>
     </html>
   );
